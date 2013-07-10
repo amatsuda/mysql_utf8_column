5867a841d09c5d269a92396f9490cd95978b6247
Akira Matsuda <ronnie@dio.jp>
2013-07-11 00:05:50 +0900

    rails new mysql_utf8_column -d mysql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MysqlUtf8Column::Application.config.secret_key_base = '9deb5cb3fab747cbd6a452007dd3dc087475e3a20fe54ba43881ce60c73f7d97b7723bb732366b1bd1c7f704df8c35c681be27b61ff4d337d732a1518d6942a7'
